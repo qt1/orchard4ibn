6d0226e9df8ce81c4aaf47222d0267200662effb
qt1 <baruch@ibn-labs.com>
2014-10-09 15:47:24 +0300

    restore build scripts

--- ClickToBuild.cmd
+++ ClickToBuild.cmd
@@ -28,36 +28,8 @@ FOR %%v in (12 11) do (
 )
   
 echo "Unable to detect suitable environment. Build may not succeed."
-<<<<<<< HEAD
 
 :build
-=======
-goto build
-
-
-:initialize2k8Dev12
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
-goto build
-
-:initialize2k8on64Dev12
-call "%ProgramFiles%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
-goto build
-
-:initialize2k8Dev11
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-goto build
-
-:initialize2k8on64Dev11
-call "%ProgramFiles%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-goto build
-
-:build
-if "%~1"=="" msbuild /t:Build Orchard.proj
-msbuild /t:%~1 Orchard.proj
-
-pause
-goto end
->>>>>>> Orchard@Codeplex/1.8.x
 
 call build %*
 
--- build.cmd
+++ build.cmd
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 if "%~1"=="" build Build
 if "%~2"=="" build %1 Orchard.proj
 msbuild /t:%~1 %2
 
-=======
-if "%~1"=="" call clicktobuild 
-call clicktobuild %~1 
->>>>>>> Orchard@Codeplex/1.8.x
